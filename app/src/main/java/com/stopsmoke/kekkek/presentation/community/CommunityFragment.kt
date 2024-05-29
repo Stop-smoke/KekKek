9c06967eb1863f3b8c29b0d9afff543f6117d1c4
yang dong won <didehddnjs89@gmail.com>
2024-05-29 14:22:42 +0900

    fead: spinner 기능 추가 디자인 설정해줘야 함, chore: fragment 디펜던시 추가

--- app/src/main/java/com/stopsmoke/kekkek/presentation/community/CommunityFragment.kt
+++ app/src/main/java/com/stopsmoke/kekkek/presentation/community/CommunityFragment.kt
@@ -5,9 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.tabs.TabLayoutMediator
+import com.stopsmoke.kekkek.R
 import com.stopsmoke.kekkek.databinding.FragmentCommunityBinding
 import com.stopsmoke.kekkek.presentation.my.MyViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -74,6 +77,46 @@ class CommunityFragment : Fragment() {
         TabLayoutMediator(tabLayoutCommunityNotice, vpCommunityNotice){ tab, position -> }.attach()
 
         viewPagerAutomaticFlippingJob
+
+        initSpinner()
+
+    }
+
+
+    private fun initSpinner() = with(binding){
+        val category: Array<String> = resources.getStringArray(R.array.category)
+        spnCommunityCategory.adapter = ArrayAdapter.createFromResource(requireContext(), R.array.category, android.R.layout.simple_spinner_item)
+        spnCommunityCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    0 -> {
+                        tvCommunitySelectCategory.text = category[0]
+                    }
+
+                    1 -> {
+                        tvCommunitySelectCategory.text = category[1]
+                    }
+
+                    2 -> {
+                        tvCommunitySelectCategory.text = category[2]
+                    }
+
+                    3 -> {
+                        tvCommunitySelectCategory.text = category[3]
+                    }
+
+                    4 -> {
+                        tvCommunitySelectCategory.text = category[4]
+                    }
+
+                    5 -> {
+                        tvCommunitySelectCategory.text = category[5]
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
     }
 
 }
