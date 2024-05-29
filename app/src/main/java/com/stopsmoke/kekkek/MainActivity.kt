f1c16b443f7ced9506cdeb9d37b0a4d8b4d502d4
SEYOUNG PARK <seyeongb18@gmail.com>
2024-05-29 15:12:39 +0900

    feat: bottom navigation view, home fragment 제작

--- app/src/main/java/com/stopsmoke/kekkek/MainActivity.kt
+++ app/src/main/java/com/stopsmoke/kekkek/MainActivity.kt
@@ -5,18 +5,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import androidx.fragment.app.Fragment
+import com.stopsmoke.kekkek.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.clMain) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemGestures())
+            view.updatePadding(0, insets.top,0, 0)
+            WindowInsetsCompat.CONSUMED
         }
+
+        setupBottomNavigation()
+        if(savedInstanceState == null) {
+            binding.bottomNavigationViewHome.selectedItemId = R.id.home
+        }
+    }
+
+    private fun setupBottomNavigation() {
+        binding.bottomNavigationViewHome.setOnItemSelectedListener { menu ->
+            when(menu.itemId) {
+                R.id.home -> {
+                    HomeFragment().changeFragment()
+                }
+                R.id.community -> {
+//                    CommunityFragment().changeFragment()
+                }
+                R.id.my_page -> {
+//                    MyPageFragment().changeFragment()
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    private fun Fragment.changeFragment() {
+        supportFragmentManager.beginTransaction().replace(R.id.fl_main,this).commit()
     }
 }
