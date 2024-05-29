f5c75e3a82239c75c5fa4cf5adb6de5affca7cd1
agvber <agvber12@gmail.com>
2024-05-29 20:16:10 +0900

    feat: firestore user 데이터 부분 추가

---
+++ app/src/main/java/com/stopsmoke/kekkek/firestore/FireStoreService.kt
@@ -0,0 +1,45 @@
+package com.stopsmoke.kekkek.firestore
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.stopsmoke.kekkek.firestore.model.UserEntity
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class FireStoreService @Inject constructor(
+    private val firestore: FirebaseFirestore,
+) {
+
+    fun getUser(userId: String): Flow<Result<UserEntity>> = callbackFlow {
+
+        val snapshotListener = firestore.collection(USER_COLLECTION)
+            .document(userId)
+            .addSnapshotListener { value, error ->
+                if (error != null) {
+                    trySendBlocking(Result.failure(error))
+                    return@addSnapshotListener
+                }
+                val user = value?.toObject(UserEntity::class.java) ?: return@addSnapshotListener
+                trySendBlocking(Result.success(user))
+            }
+
+        awaitClose {
+            snapshotListener.remove()
+        }
+    }
+
+    suspend fun setUser(userEntity: UserEntity) {
+        firestore.collection(USER_COLLECTION).document(userEntity.id ?: return)
+            .set(userEntity)
+            .addOnFailureListener { throw it }
+            .await()
+    }
+
+
+    companion object {
+        private const val USER_COLLECTION = "user"
+    }
+}
