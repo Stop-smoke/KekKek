f05ac06f7e2a7798bb138caba13505aa22416987
agvber <agvber12@gmail.com>
2024-05-31 14:53:51 +0900

    feat: ranking 데이터 추가

--- app/src/main/java/com/stopsmoke/kekkek/firestore/di/FireStoreDaoModule.kt
+++ app/src/main/java/com/stopsmoke/kekkek/firestore/di/FireStoreDaoModule.kt
@@ -2,8 +2,10 @@ package com.stopsmoke.kekkek.firestore.di
 
 import com.stopsmoke.kekkek.firestore.dao.CommentDao
 import com.stopsmoke.kekkek.firestore.dao.PostDao
+import com.stopsmoke.kekkek.firestore.dao.RankingDao
 import com.stopsmoke.kekkek.firestore.data.CommentDaoImpl
 import com.stopsmoke.kekkek.firestore.data.PostDaoImpl
+import com.stopsmoke.kekkek.firestore.data.RankingDaoImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,11 +17,16 @@ internal interface FireStoreDaoModule {
 
     @Binds
     fun bindPostDao(
-        postDaoImpl: PostDaoImpl
+        postDaoImpl: PostDaoImpl,
     ): PostDao
 
     @Binds
     fun bindCommentDao(
-        commentDaoImpl: CommentDaoImpl
+        commentDaoImpl: CommentDaoImpl,
     ): CommentDao
+
+    @Binds
+    fun bindRankingDao(
+        rankingDaoImpl: RankingDaoImpl,
+    ): RankingDao
 }
