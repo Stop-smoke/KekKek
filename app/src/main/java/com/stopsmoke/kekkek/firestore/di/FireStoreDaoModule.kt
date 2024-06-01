958a893f828fa13196b8f87b969fc5be8eed5072
agvber <agvber12@gmail.com>
2024-06-01 17:07:06 +0900

    feat: userDao 의존성 추가

--- app/src/main/java/com/stopsmoke/kekkek/firestore/di/FireStoreDaoModule.kt
+++ app/src/main/java/com/stopsmoke/kekkek/firestore/di/FireStoreDaoModule.kt
@@ -3,9 +3,11 @@ package com.stopsmoke.kekkek.firestore.di
 import com.stopsmoke.kekkek.firestore.dao.CommentDao
 import com.stopsmoke.kekkek.firestore.dao.PostDao
 import com.stopsmoke.kekkek.firestore.dao.RankingDao
+import com.stopsmoke.kekkek.firestore.dao.UserDao
 import com.stopsmoke.kekkek.firestore.data.CommentDaoImpl
 import com.stopsmoke.kekkek.firestore.data.PostDaoImpl
 import com.stopsmoke.kekkek.firestore.data.RankingDaoImpl
+import com.stopsmoke.kekkek.firestore.data.UserDaoImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -29,4 +31,9 @@ internal interface FireStoreDaoModule {
     fun bindRankingDao(
         rankingDaoImpl: RankingDaoImpl,
     ): RankingDao
+
+    @Binds
+    fun bindUserDao(
+        userDaoImpl: UserDaoImpl,
+    ): UserDao
 }
