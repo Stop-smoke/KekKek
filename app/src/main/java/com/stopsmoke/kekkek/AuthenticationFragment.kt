9e14a14273635c3ed048d387840f32e1a9ff5523
agvber <agvber12@gmail.com>
2024-06-02 01:44:49 +0900

    feat: 구글 로그인 기능 추가

--- app/src/main/java/com/stopsmoke/kekkek/AuthenticationFragment.kt
+++ app/src/main/java/com/stopsmoke/kekkek/AuthenticationFragment.kt
@@ -17,40 +17,35 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.oAuthCredential
+import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.model.ClientError
-import com.kakao.sdk.common.model.ClientErrorCause
-import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 import com.stopsmoke.kekkek.databinding.FragmentAuthenticationBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AuthenticationFragment : Fragment() {
 
     private var _binding: FragmentAuthenticationBinding? = null
     private val binding: FragmentAuthenticationBinding get() = _binding!!
 
-    private val firebaseAuth: FirebaseAuth by lazy {
-        FirebaseAuth.getInstance()
-    }
-
-    private val googleSighInClient: GoogleSignInClient by lazy {
-        GoogleSignIn.getClient(
-            requireContext(), GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("963210879703-ocaf574hbvf2gq9hahpmhhvmbgbvn50s.apps.googleusercontent.com")
-                .requestId()
-                .requestEmail()
-                .requestProfile()
-                .build()
-        )
-    }
-
+    private lateinit var googleSighInClient: GoogleSignInClient
     private lateinit var googleLoginResult: ActivityResultLauncher<Intent>
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+        registerGoogleAuthRequest()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        initRegister()
         _binding = FragmentAuthenticationBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -58,11 +53,8 @@ class AuthenticationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var keyHash = Utility.getKeyHash(requireContext())
-        Log.e("key", "해시 키 값 : ${keyHash}")
-
         binding.ivAuthKakaoLogin.setOnClickListener {
-            clickKakaoLoginButton()
+            loginKakao()
         }
 
         binding.ivAuthGoogleLogin.setOnClickListener {
@@ -70,105 +62,126 @@ class AuthenticationFragment : Fragment() {
         }
     }
 
-    private fun clickGoogleLoginButton() {
-        val signInIntent = googleSighInClient.signInIntent
-        googleLoginResult.launch(signInIntent)
-    }
-
-
-    private fun clickKakaoLoginButton() {
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            if (error != null) {
-                Log.e("kakao", "카카오계정으로 로그인 실패", error)
-            } else if (token != null) {
-                Log.i("kakao", "카카오계정으로 로그인 성공 ${token.accessToken}")
-            }
-        }
-
-        if (UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
-            UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
-                if (error != null) {
-                    Log.e("kakao1", "카카오톡으로 로그인 실패", error)
-                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
-                        Log.e("kakao2", "${error.reason}")
-                        return@loginWithKakaoTalk
-                    }
-                    UserApiClient.instance.loginWithKakaoAccount(
-                        requireContext(),
-                        callback = callback
-                    )
-                } else if (token != null) {
-                    Log.i("kakao", "카카오톡으로 로그인 성공 ${token.accessToken}")
-                    UserApiClient.instance.me { user, error ->
-                        if (error != null) {
-                            Log.e("kakaouser", "사용자 정보 요청 실패", error)
-                        } else if (user != null) { // kakao developer에서 동의항목 변경 시 조회
-                            Log.i(
-                                "kakaouser", "사용자 정보 요청 성공" +
-                                        "\n회원번호: ${user.id}" +
-                                        "\n이메일: ${user.kakaoAccount?.email}" +
-                                        "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
-                                        "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}"
-                            )
-                        }
-                    }
-                }
-            }
-        } else UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
-    }
-
-    private fun initRegister() {
+    private fun registerGoogleAuthRequest() {
         googleLoginResult =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 if (result.resultCode == Activity.RESULT_OK) {
-                    val data = result.data
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
                     try {
-                        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-                        val account = task.getResult(ApiException::class.java)
-                        firebaseAuthWithGoogle(account.idToken)
+                        // Google Sign In was successful, authenticate with Firebase
+                        val account = task.getResult(ApiException::class.java)!!
+                        Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                        firebaseAuthWithGoogle(account.idToken!!)
                     } catch (e: ApiException) {
-                        // Google 로그인 실패 처리
-                        Log.d("google", "Google 로그인 실패: ${e.statusCode}")
+                        // Google Sign In failed, update UI appropriately
+                        Log.w(TAG, "Google sign in failed", e)
+                        e.printStackTrace()
                     }
-                } else {
-                    // 사용자가 로그인을 취소했거나 결과가 OK가 아닐 때의 처리
-                    Log.d("오류", "로그인 사용자 취소 또는 실패: ${result.resultCode}")
                 }
             }
     }
 
-    private fun firebaseAuthWithGoogle(idToken: String?) {
+    private fun clickGoogleLoginButton() {
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(BuildConfig.FIRBASE_AUTH_SERVCER_CLIENT_KEY)
+            .requestId()
+            .requestEmail()
+            .requestProfile()
+            .build()
+
+        googleSighInClient = GoogleSignIn.getClient(requireContext(), options)
+        googleLoginResult.launch(googleSighInClient.signInIntent)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
-        firebaseAuth.signInWithCredential(credential)
-            .addOnSuccessListener { result ->
-                val user = result.user
-                Log.d("googleuser", "$user")
-                // Handle successful sign in here
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    Log.d("Google 로그인", user.toString())
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+            }
+            .addOnFailureListener {
+                it.printStackTrace()
+            }
+    }
+
+    private fun loginKakao() {
+        if (!getKakaoAppLoginAvailable()) {
+            loginKakaoWeb()
+            return
+        }
+        UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
+            if (error != null || token == null) {
+                Log.e("kakao", "카카오톡으로 로그인 실패", error)
+                loginKakaoWeb()
+                return@loginWithKakaoTalk
+            }
+
+            UserApiClient.instance.me { user, error ->
+                if (error != null || user == null) {
+                    Log.e("kakaouser", "사용자 정보 요청 실패", error)
+                    return@me
+                }
+                token.registerKakaoToken()
+                showSuccessLog(user)
+            }
+            Log.i("kakao", "카카오톡으로 로그인 성공 ${token.accessToken}")
+        }
+    }
+
+    private fun getKakaoAppLoginAvailable() =
+        UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())
+
+    private fun loginKakaoWeb() {
+        UserApiClient.instance.loginWithKakaoAccount(
+            context = requireContext(),
+            callback = { token, error ->
+                if (error != null) {
+                    Log.e("kakao", "카카오계정으로 로그인 실패", error)
+                } else if (token != null) {
+                    token.registerKakaoToken()
+                    Log.i("kakao", "카카오계정으로 로그인 성공 ${token.accessToken}")
+                }
+            }
+        )
+    }
+
+    private fun showSuccessLog(
+        user: com.kakao.sdk.user.model.User,
+    ) {
+        Log.i(
+            "kakaouser", "사용자 정보 요청 성공" +
+                    "\n회원번호: ${user.id}" +
+                    "\n이메일: ${user.kakaoAccount?.email}" +
+                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                    "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}"
+        )
+    }
+
+    private fun OAuthToken.registerKakaoToken() {
+        val providerId = "oidc.kakao"
+        val credential = oAuthCredential(providerId) {
+            idToken = this@registerKakaoToken.idToken
+            accessToken = this@registerKakaoToken.accessToken
+        }
+
+        auth.signInWithCredential(credential)
+            .addOnSuccessListener { authResult ->
+                authResult.user?.displayName?.let {
+                    Toast.makeText(requireContext(), "${it}님 환영합니다", Toast.LENGTH_SHORT).show()
+                }
             }
             .addOnFailureListener { e ->
-                Log.e("googleuser", "Google sign in failed", e)
-                // Handle failure here
+                Toast.makeText(requireContext(), "로그인을 실패하였습니다", Toast.LENGTH_SHORT).show()
+                e.printStackTrace()
             }
-    }
-
-    private fun signOut() {
-        // Firebase sign out
-        firebaseAuth.signOut()
-
-        // Google sign out
-        googleSighInClient.signOut().addOnCompleteListener(requireActivity()) {
-            Toast.makeText(requireContext(), "Complete", Toast.LENGTH_LONG).show()
-        }
-    }
-
-    private fun revokeAccess() {
-        // Firebase sign out
-        firebaseAuth.signOut()
-
-        // Google revoke access
-        googleSighInClient.revokeAccess().addOnCompleteListener(requireActivity()) {
-            Toast.makeText(requireContext(), "Complete", Toast.LENGTH_LONG).show()
-        }
     }
 
     override fun onDestroyView() {
@@ -179,5 +192,7 @@ class AuthenticationFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() = AuthenticationFragment()
+
+        private const val TAG = "AuthenticationFragment"
     }
 }
