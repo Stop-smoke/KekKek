3438e2e55e2d660111e54b24848f08c72a2304c6
SEYOUNG PARK <seyeongb18@gmail.com>
2024-06-02 19:15:44 +0900

    fix: 레이아웃 수정, string 추가

--- app/src/main/java/com/stopsmoke/kekkek/presentation/MainActivity.kt
+++ app/src/main/java/com/stopsmoke/kekkek/presentation/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
 
