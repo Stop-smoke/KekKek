152b567bd3abb0901e1cff1a3ef98e3d28a4e17c
SEYOUNG PARK <seyeongb18@gmail.com>
2024-06-04 20:45:02 +0900

    refractor: 패키지 이동

--- app/src/main/java/com/stopsmoke/kekkek/post/PostViewFragment.kt
+++ app/src/main/java/com/stopsmoke/kekkek/post/PostViewFragment.kt
@@ -1,4 +1,4 @@
-package com.stopsmoke.kekkek
+package com.stopsmoke.kekkek.post
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 //import com.google.android.gms.ads.AdRequest
 //import com.google.android.gms.ads.MobileAds
 import com.stopsmoke.kekkek.databinding.FragmentPostViewBinding
+import com.stopsmoke.kekkek.invisible
 
 
 class PostViewFragment : Fragment() {
