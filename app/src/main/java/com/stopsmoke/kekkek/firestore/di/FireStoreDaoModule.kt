a8b2e6aaf3f34d5ac5cc8b3b8170a740b2d3ad77
agvber <agvber12@gmail.com>
2024-06-04 13:56:58 +0900

    feat: NotificationDao 의존성 주입

--- app/src/main/java/com/stopsmoke/kekkek/firestore/di/FireStoreDaoModule.kt
+++ app/src/main/java/com/stopsmoke/kekkek/firestore/di/FireStoreDaoModule.kt
@@ -1,10 +1,12 @@
 package com.stopsmoke.kekkek.firestore.di
 
 import com.stopsmoke.kekkek.firestore.dao.CommentDao
+import com.stopsmoke.kekkek.firestore.dao.NotificationDao
 import com.stopsmoke.kekkek.firestore.dao.PostDao
 import com.stopsmoke.kekkek.firestore.dao.RankingDao
 import com.stopsmoke.kekkek.firestore.dao.UserDao
 import com.stopsmoke.kekkek.firestore.data.CommentDaoImpl
+import com.stopsmoke.kekkek.firestore.data.NotificationDaoImpl
 import com.stopsmoke.kekkek.firestore.data.PostDaoImpl
 import com.stopsmoke.kekkek.firestore.data.RankingDaoImpl
 import com.stopsmoke.kekkek.firestore.data.UserDaoImpl
@@ -36,4 +38,9 @@ internal interface FireStoreDaoModule {
     fun bindUserDao(
         userDaoImpl: UserDaoImpl,
     ): UserDao
+
+    @Binds
+    fun bindNotificationDao(
+        notificationDaoImpl: NotificationDaoImpl,
+    ): NotificationDao
 }
