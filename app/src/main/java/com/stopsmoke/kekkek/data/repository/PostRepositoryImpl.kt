488d49e2a9e98fb09d4fcc02b2ac70736885e8ff
agvber <agvber12@gmail.com>
2024-06-07 02:01:25 +0900

    refactor: 옵저빙 가능한 타입 값으로 변경

--- app/src/main/java/com/stopsmoke/kekkek/data/repository/PostRepositoryImpl.kt
+++ app/src/main/java/com/stopsmoke/kekkek/data/repository/PostRepositoryImpl.kt
@@ -2,25 +2,32 @@ package com.stopsmoke.kekkek.data.repository
 
 import androidx.paging.PagingData
 import androidx.paging.map
+import com.stopsmoke.kekkek.common.Result
 import com.stopsmoke.kekkek.data.mapper.asExternalModel
 import com.stopsmoke.kekkek.domain.model.Post
 import com.stopsmoke.kekkek.domain.repository.PostRepository
 import com.stopsmoke.kekkek.firestore.dao.PostDao
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 internal class PostRepositoryImpl @Inject constructor(
     private val postDao: PostDao,
 ) : PostRepository {
-    override fun getPost(categories: List<String>): Result<Flow<PagingData<Post>>> {
-        return kotlin.runCatching {
-            postDao.getPost(categories = categories)
-                .map { pagingData ->
-                    pagingData.map {
-                        it.asExternalModel()
-                    }
+    override fun getPost(categories: List<String>): Flow<Result<PagingData<Post>>> = try {
+        postDao.getPost(categories = categories)
+            .map { pagingData ->
+                pagingData.map {
+                    it.asExternalModel()
                 }
-        }
+                    .let {
+                        Result.Success(it)
+                    }
+            }
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+        flowOf(Result.Error(e))
     }
 }
--- app/src/main/java/com/stopsmoke/kekkek/domain/repository/PostRepository.kt
+++ app/src/main/java/com/stopsmoke/kekkek/domain/repository/PostRepository.kt
@@ -1,10 +1,11 @@
 package com.stopsmoke.kekkek.domain.repository
 
 import androidx.paging.PagingData
+import com.stopsmoke.kekkek.common.Result
 import com.stopsmoke.kekkek.domain.model.Post
 import kotlinx.coroutines.flow.Flow
 
 interface PostRepository {
 
-    fun getPost(categories: List<String> = emptyList()): Result<Flow<PagingData<Post>>>
+    fun getPost(categories: List<String> = emptyList()): Flow<Result<PagingData<Post>>>
 }
