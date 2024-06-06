c50b38c43ff506cdf35b2ac2dc07c52df79f3013
agvber <agvber12@gmail.com>
2024-06-06 15:28:56 +0900

    feat: 게시글 model 클래스 추가

---
+++ app/src/main/java/com/stopsmoke/kekkek/domain/model/Post.kt
@@ -0,0 +1,27 @@
+package com.stopsmoke.kekkek.domain.model
+
+import com.stopsmoke.kekkek.domain.getElapsedDateTime
+
+data class Post(
+    val id: String,
+    val commentId: String,
+    val written: Written,
+    val title: String,
+    val text: String,
+    val dateTime: DateTime,
+    val likeUser: List<String>,
+    val unlikeUser: List<String>,
+    val categories: PostCategory,
+    val views: Long,
+    val commentUser: List<String>,
+) {
+    val modifiedElapsedDateTime = dateTime.modified.getElapsedDateTime()
+
+    data class Written(
+        val uid: String,
+        val name: String,
+        val profileImage: ProfileImage,
+        val ranking: Long,
+    )
+
+}
