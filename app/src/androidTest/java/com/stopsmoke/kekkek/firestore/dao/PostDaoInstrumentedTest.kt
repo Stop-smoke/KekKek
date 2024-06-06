d49df3482d6e0ea3104e09d0c21d5f86c4a9459c
agvber <agvber12@gmail.com>
2024-06-06 15:32:12 +0900

    feat: firebase post 엔티티 최신 버전 대응

--- app/src/androidTest/java/com/stopsmoke/kekkek/firestore/dao/PostDaoInstrumentedTest.kt
+++ app/src/androidTest/java/com/stopsmoke/kekkek/firestore/dao/PostDaoInstrumentedTest.kt
@@ -2,9 +2,11 @@ package com.stopsmoke.kekkek.firestore.dao
 
 import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.stopsmoke.kekkek.firestore.data.PostDaoImpl
+import com.stopsmoke.kekkek.firestore.model.DateTimeEntity
 import com.stopsmoke.kekkek.firestore.model.PostEntity
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
@@ -12,6 +14,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.time.LocalDateTime
+import kotlin.random.Random
 
 @RunWith(AndroidJUnit4::class)
 class PostDaoInstrumentedTest {
@@ -32,18 +35,25 @@ class PostDaoInstrumentedTest {
     @Test // 게시글 등록
     fun addPost() = runTest {
         val post = PostEntity(
-            id = "12345",
-            title = "Sample Post",
-            text = "This is a sample post for testing purposes.",
-            dateTime = LocalDateTime.now().toString(),
-            bookmark = 10,
-            isBookmark = true,
-            like = 50,
-            isLike = true,
-            unlike = 2,
-            isUnlike = false,
-            commentId = "cmt12345",
-            writtenUid = "user123"
+            id = "dummyId",
+            commentId = "dummyCommentId",
+            written = PostEntity.Written(
+                uid = "default",
+                name = "default",
+                profileImage = "https://file.notion.so/f/f/d2c6a42f-73c9-4a08-a064-629644e1df36/5dd968b8-2ed6-4a11-835d-a3bd71618151/Untitled.png?id=afb9acf4-f456-4b1b-8ab4-8e0bcd14002b&table=block&spaceId=d2c6a42f-73c9-4a08-a064-629644e1df36&expirationTimestamp=1717675200000&signature=WV4mFdY8JbZ45ZOBtMFcR5pMD_VrxqbFY7GlDCL0SU0&downloadName=Untitled.png",
+                ranking = Random.nextLong(1, 500),
+            ),
+            title = "Dummy Title",
+            text = "Dummy Text",
+            dateTime = DateTimeEntity(
+                created = Timestamp.now(),
+                modified = Timestamp.now(),
+            ),
+            likeUser = listOf("likeUser1", "likeUser2"),
+            unlikeUser = listOf("unlikeUser1", "unlikeUser2"),
+            categories = "dummyCategory",
+            views = Random.nextLong(1, 500),
+            commentUser = listOf("commentUser1", "commentUser2")
         )
         postDao.addPost(post)
     }
@@ -51,18 +61,25 @@ class PostDaoInstrumentedTest {
     @Test
     fun updatePost() = runTest {
         val post = PostEntity(
-            id = "lOMR60vdOoIx2gGSjZr2",
-            title = "Sample Post 1",
-            text = "This is a sample post for testing purposes.",
-            dateTime = LocalDateTime.now().toString(),
-            bookmark = 10,
-            isBookmark = true,
-            like = 50,
-            isLike = true,
-            unlike = 2,
-            isUnlike = false,
-            commentId = "cmt12345",
-            writtenUid = "user123"
+            id = "dummyId",
+            commentId = "dummyCommentId",
+            written = PostEntity.Written(
+                uid = "default",
+                name = "default",
+                profileImage = "https://file.notion.so/f/f/d2c6a42f-73c9-4a08-a064-629644e1df36/5dd968b8-2ed6-4a11-835d-a3bd71618151/Untitled.png?id=afb9acf4-f456-4b1b-8ab4-8e0bcd14002b&table=block&spaceId=d2c6a42f-73c9-4a08-a064-629644e1df36&expirationTimestamp=1717675200000&signature=WV4mFdY8JbZ45ZOBtMFcR5pMD_VrxqbFY7GlDCL0SU0&downloadName=Untitled.png",
+                ranking = Random.nextLong(1, 500),
+            ),
+            title = "Dummy Title",
+            text = "Dummy Text",
+            dateTime = DateTimeEntity(
+                created = Timestamp.now(),
+                modified = Timestamp.now(),
+            ),
+            likeUser = listOf("likeUser1", "likeUser2"),
+            unlikeUser = listOf("unlikeUser1", "unlikeUser2"),
+            categories = "dummyCategory",
+            views = Random.nextLong(1, 500),
+            commentUser = listOf("commentUser1", "commentUser2")
         )
         postDao.updateOrInsertPost(post)
     }
