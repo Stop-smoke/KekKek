ecd3d5fb8d6aa4d046719ad57ae58dc09c5a1097
agvber <agvber12@gmail.com>
2024-06-06 15:29:39 +0900

    feat: 게시글 서버 로드되는 함수 추가

--- app/src/main/java/com/stopsmoke/kekkek/domain/repository/PostRepository.kt
+++ app/src/main/java/com/stopsmoke/kekkek/domain/repository/PostRepository.kt
@@ -1,4 +1,10 @@
 package com.stopsmoke.kekkek.domain.repository
 
+import androidx.paging.PagingData
+import com.stopsmoke.kekkek.domain.model.Post
+import kotlinx.coroutines.flow.Flow
+
 interface PostRepository {
+
+    fun getPost(categories: List<String> = emptyList()): Result<Flow<PagingData<Post>>>
 }
