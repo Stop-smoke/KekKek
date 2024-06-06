e5a35a369634ad7bbec0cea17eace4c94f355061
agvber <agvber12@gmail.com>
2024-06-07 01:52:02 +0900

    chore: androidx.fragment 버전 카탈로그 저장

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     // CORE
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity)
-    implementation("androidx.fragment:fragment-ktx:1.7.1")
+    implementation(libs.androidx.fragment.ktx)
 
     // UI
     implementation(libs.androidx.appcompat)
