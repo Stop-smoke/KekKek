395a2a5726f46ee5bcaf84d36097b0c41b49d2a9
agvber <agvber12@gmail.com>
2024-06-07 01:59:35 +0900

    feat: CommentRepository 의존성 주입

--- app/src/main/java/com/stopsmoke/kekkek/data/di/DataModule.kt
+++ app/src/main/java/com/stopsmoke/kekkek/data/di/DataModule.kt
@@ -1,9 +1,11 @@
 package com.stopsmoke.kekkek.data.di
 
+import com.stopsmoke.kekkek.data.repository.CommentRepositoryImpl
 import com.stopsmoke.kekkek.data.repository.NotificationRepositoryImpl
 import com.stopsmoke.kekkek.data.repository.PostRepositoryImpl
 import com.stopsmoke.kekkek.data.repository.SearchRepositoryImpl
 import com.stopsmoke.kekkek.data.repository.UserRepositoryImpl
+import com.stopsmoke.kekkek.domain.repository.CommentRepository
 import com.stopsmoke.kekkek.domain.repository.NotificationRepository
 import com.stopsmoke.kekkek.domain.repository.PostRepository
 import com.stopsmoke.kekkek.domain.repository.SearchRepository
@@ -36,4 +38,9 @@ internal interface DataModule {
     fun bindsPostRepository(
         postRepositoryImpl: PostRepositoryImpl,
     ): PostRepository
+
+    @Binds
+    fun bindCommentRepository(
+        commentRepositoryImpl: CommentRepositoryImpl
+    ): CommentRepository
 }
