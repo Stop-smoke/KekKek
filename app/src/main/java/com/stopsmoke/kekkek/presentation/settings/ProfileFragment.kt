8d595e0dba25da09138607a68935d5e589d42801
SEYOUNG PARK <seyeongb18@gmail.com>
2024-06-07 14:32:50 +0900

    feat: 계정 설정 부분 일부 구현

--- app/src/main/java/com/stopsmoke/kekkek/presentation/settings/ProfileFragment.kt
+++ app/src/main/java/com/stopsmoke/kekkek/presentation/settings/ProfileFragment.kt
@@ -6,18 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.stopsmoke.kekkek.R
+import com.stopsmoke.kekkek.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding ?= null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
