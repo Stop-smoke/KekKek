8431c3e0e71754de1e140712e0209ba805a84ca4
agvber <agvber12@gmail.com>
2024-06-08 01:59:07 +0900

    refactor: Navigation onboarding 코드 수정하였습니다

--- app/src/main/java/com/stopsmoke/kekkek/presentation/onboarding/OnboardingFinishFragment.kt
+++ app/src/main/java/com/stopsmoke/kekkek/presentation/onboarding/OnboardingFinishFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.stopsmoke.kekkek.databinding.FragmentOnboardingFinishBinding
 import com.stopsmoke.kekkek.presentation.DatastoreHelper
+import com.stopsmoke.kekkek.visible
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -28,9 +30,15 @@ class OnboardingFinishFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         lifecycleScope.launch {
-            delay(6000)
+            delay(3000)
             DatastoreHelper.setOnboardingComplete(requireContext(), true)
+            findNavController().navigate("home")
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        activity?.visible()
     }
 
     override fun onDestroyView() {
