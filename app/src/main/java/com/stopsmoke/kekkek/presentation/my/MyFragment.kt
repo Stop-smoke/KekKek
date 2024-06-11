b8931bf1f9491c4327dfff489350ea913839d635
yang dong won <didehddnjs89@gmail.com>
2024-06-11 23:44:53 +0900

    feat: my fragment 내가 쓴 글, 댓글, 북마크 개수 저굥

--- app/src/main/java/com/stopsmoke/kekkek/presentation/my/MyFragment.kt
+++ app/src/main/java/com/stopsmoke/kekkek/presentation/my/MyFragment.kt
@@ -15,8 +15,8 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import coil.load
 import com.stopsmoke.kekkek.R
+import com.stopsmoke.kekkek.common.Result
 import com.stopsmoke.kekkek.databinding.FragmentMyBinding
-import com.stopsmoke.kekkek.domain.model.User
 import com.stopsmoke.kekkek.visible
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -115,20 +115,25 @@ class MyFragment : Fragment() {
                 }
         }
 
-        lifecycleScope.launch {
-            userData.flowWithLifecycle(lifecycle).collectLatest {
-                when(it) {
-                    is User.Error -> {
-                        // 유저 정보가 에러
-                    }
-                    is User.Guest -> {  } // 게스트 모드 대응
-                    is User.Registered -> {
-                        viewModel.updateUserData(it)
-                    }
+
+        when (userData) {
+            is Result.Error -> {
+
+            }
+
+            is Result.Loading -> {
+
+            }
+
+            is Result.Success -> {
+                viewLifecycleOwner.lifecycleScope.launch {
+                    userData.data.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+                        .collectLatest { user ->
+                            viewModel.updateUserData(user)
+                        }
                 }
             }
         }
-        Unit
     }
 
 
