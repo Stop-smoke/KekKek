4ccf282158d161a0bbdaa7410aafc2adc0b105f4
SEYOUNG PARK <seyeongb18@gmail.com>
2024-06-13 20:11:45 +0900

    enhancement: navigation 불필요한 부분 invisible 처리

--- app/src/main/java/com/stopsmoke/kekkek/presentation/myBookmarkList/BookmarkFragment.kt
+++ app/src/main/java/com/stopsmoke/kekkek/presentation/myBookmarkList/BookmarkFragment.kt
@@ -14,6 +14,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.paging.PagingData
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
 import com.stopsmoke.kekkek.R
 import com.stopsmoke.kekkek.databinding.FragmentBookmarkBinding
 import com.stopsmoke.kekkek.invisible
@@ -91,6 +92,9 @@ class BookmarkFragment : Fragment() {
     }
 
     private fun initObserveLiveData() {
+        //
+        val db = FirebaseFirestore.getInstance()
+
         postViewModel.bookmarkPosts.observe(viewLifecycleOwner) {
             val paging = PagingData.from(it)
             viewLifecycleOwner.lifecycleScope.launch {
