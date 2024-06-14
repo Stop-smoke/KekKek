48e502ce29398ba92e1fca4743b84126f4fe301b
SEYOUNG PARK <seyeongb18@gmail.com>
2024-06-14 16:21:23 +0900

    enhancement: 글쓰기에서 카테고리 2개 더 추가(금연 실패 후기, 금연 다짐)

--- app/src/main/java/com/stopsmoke/kekkek/domain/model/PostWriteCategory.kt
+++ app/src/main/java/com/stopsmoke/kekkek/domain/model/PostWriteCategory.kt
@@ -3,11 +3,15 @@ package com.stopsmoke.kekkek.domain.model
 enum class PostWriteCategory {
     GENERAL_DISCUSSION,
     SUCCESS_STORIES,
-    QUIT_SMOKING_AIDS_REVIEWS
+    QUIT_SMOKING_AIDS_REVIEWS,
+    FAILURE_STORIES,
+    QUIT_SMOKING_WILLINGNESS
 }
 
 fun PostWriteCategory.toRequestString(): String = when(this) {
     PostWriteCategory.QUIT_SMOKING_AIDS_REVIEWS -> "quit_smoking_aids_reviews"
     PostWriteCategory.SUCCESS_STORIES -> "success_stories"
     PostWriteCategory.GENERAL_DISCUSSION -> "general_discussion"
+    PostWriteCategory.FAILURE_STORIES -> "failure_stories"
+    PostWriteCategory.QUIT_SMOKING_WILLINGNESS -> "resolutions"
 }
