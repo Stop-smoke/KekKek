4f3d0d4e38040278d5ffe1027b63702a962fcf50
agvber <agvber12@gmail.com>
2024-06-15 19:20:16 +0900

    style: 라인 및 코드 정리

--- app/src/main/java/com/stopsmoke/kekkek/firestore/data/CommentDaoImpl.kt
+++ app/src/main/java/com/stopsmoke/kekkek/firestore/data/CommentDaoImpl.kt
@@ -15,7 +15,6 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
-import java.util.concurrent.CancellationException
 import javax.inject.Inject
 
 class CommentDaoImpl @Inject constructor(
@@ -91,38 +90,32 @@ class CommentDaoImpl @Inject constructor(
     }
 
 
-        override suspend fun addComment(commentEntity: CommentEntity) {
-            firestore
-                .collection(COLLECTION)
-                .document().let { documentReference ->
-                    documentReference.set(
-                        commentEntity.copy(id = documentReference.id)
-                    )
-                }
-                .addOnFailureListener { throw it }
-                .addOnCanceledListener { throw CancellationException() }
-                .await()
-        }
+    override suspend fun addComment(commentEntity: CommentEntity) {
+        firestore
+            .collection(COLLECTION)
+            .document().let { documentReference ->
+                documentReference.set(
+                    commentEntity.copy(id = documentReference.id)
+                )
+            }
+            .await()
+    }
 
-        override suspend fun updateOrInsertComment(commentEntity: CommentEntity) {
-            firestore
-                .collection(COLLECTION)
-                .document(commentEntity.id!!)
-                .set(commentEntity)
-                .addOnFailureListener { throw it }
-                .addOnCanceledListener { throw CancellationException() }
-                .await()
-        }
+    override suspend fun updateOrInsertComment(commentEntity: CommentEntity) {
+        firestore
+            .collection(COLLECTION)
+            .document(commentEntity.id!!)
+            .set(commentEntity)
+            .await()
+    }
 
-        override suspend fun deleteComment(commentId: String) {
-            firestore
-                .collection(COLLECTION)
-                .document(commentId)
-                .delete()
-                .addOnFailureListener { throw it }
-                .addOnCanceledListener { throw CancellationException() }
-                .await()
-        }
+    override suspend fun deleteComment(commentId: String) {
+        firestore
+            .collection(COLLECTION)
+            .document(commentId)
+            .delete()
+            .await()
+    }
 
     override fun getCommentCount(postId: String): Flow<Long> = callbackFlow {
         firestore.collection(COLLECTION)
@@ -141,7 +134,7 @@ class CommentDaoImpl @Inject constructor(
     }
 
     companion object {
-            private const val COLLECTION = "comment"
-            private const val PAGE_LIMIT = 30
-        }
+        private const val COLLECTION = "comment"
+        private const val PAGE_LIMIT = 30
     }
+}
