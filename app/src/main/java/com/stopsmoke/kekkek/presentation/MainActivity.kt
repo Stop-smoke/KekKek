9e26d0afacc84b5c4b708af957589849a37e4bd4
xeejin <heezeenee@gmail.com>
2024-06-18 18:58:54 +0900

    design: 바텀 네비게이션을 컨스트레인트로 구현 후 커스텀

--- app/src/main/java/com/stopsmoke/kekkek/presentation/MainActivity.kt
+++ app/src/main/java/com/stopsmoke/kekkek/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.stopsmoke.kekkek.presentation
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -61,29 +62,79 @@ class MainActivity : AppCompatActivity() {
         setNavGraph(isFirstRunning)
     }
 
-    private fun setupBottomNavigation() = with(binding.bottomNavigationViewHome) {
-        setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home -> {
-                    navController.popBackStack(route = "home", inclusive = false)
+
+    private fun setupBottomNavigation() {
+
+        val navItems = listOf(
+            binding.navHome to Pair(binding.ivNavHome, binding.tvNavHome),
+            binding.navCommunity to Pair(binding.ivNavCommunity, binding.tvNavCommunity),
+            binding.navMypage to Pair(binding.ivNavMypage, binding.tvNavMypage)
+        )
+
+        val unselectedColor = ContextCompat.getColor(this, R.color.gray_lightgray2)
+        val selectedColor = ContextCompat.getColor(this, R.color.black)
+
+        fun selectNavItem(selectedItem: View) {
+            navItems.forEach { (itemLayout, views) ->
+                val (imageView, textView) = views
+                if (itemLayout == selectedItem) {
+                    imageView.setColorFilter(selectedColor)
+                    textView.setTextColor(selectedColor)
+                } else {
+                    imageView.setColorFilter(unselectedColor)
+                    textView.setTextColor(unselectedColor)
                 }
 
-                R.id.community -> {
-                    navController.popBackStack(route = "community", inclusive = false)
-                }
-
-                R.id.my_page -> {
-                    navController.popBackStack(route = "my", inclusive = false)
-                }
             }
-            item.onNavDestinationSelected(navController)
         }
 
-        itemIconTintList =
-            ContextCompat.getColorStateList(this@MainActivity, R.color.bottom_nav_color)
-        itemTextColor =
-            ContextCompat.getColorStateList(this@MainActivity, R.color.bottom_nav_color)
+        navItems.forEach { (itemLayout, _) ->
+            itemLayout.setOnClickListener {
+                when (itemLayout.id) {
+                    R.id.nav_home -> {
+                        navController.navigate(R.id.home)
+
+                    }
+                    R.id.nav_community -> {
+                        navController.navigate(R.id.community)
+
+                    }
+                    R.id.nav_mypage -> {
+                        navController.navigate(R.id.my_page)
+
+                    }
+                }
+                selectNavItem(itemLayout)
+            }
+        }
+        selectNavItem(binding.navHome)
     }
+
+
+//    기존 바텀 네비게이션 사용시 사용했던 코드
+//    private fun setupBottomNavigation() = with(binding.bottomNavigationViewHome) {
+//        setOnItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.home -> {
+//                    navController.popBackStack(route = "home", inclusive = false)
+//                }
+//
+//                R.id.community -> {
+//                    navController.popBackStack(route = "community", inclusive = false)
+//                }
+//
+//                R.id.my_page -> {
+//                    navController.popBackStack(route = "my", inclusive = false)
+//                }
+//            }
+//            item.onNavDestinationSelected(navController)
+//        }
+//
+//        itemIconTintList =
+//            ContextCompat.getColorStateList(this@MainActivity, R.color.bottom_nav_color)
+//        itemTextColor =
+//            ContextCompat.getColorStateList(this@MainActivity, R.color.bottom_nav_color)
+//    }
 
     private fun setNavGraph(isAlreadyLogin: Boolean) {
         val navGraph = navController.navInflater.inflate(R.navigation.nav_graph)
