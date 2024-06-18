170e3764e84732fd7fad65272222ae57208381a5
yang dong won <didehddnjs89@gmail.com>
2024-06-18 16:42:10 +0900

    fix: 글쓰기 후 자기가 쓴 글 로딩

--- app/src/main/java/com/stopsmoke/kekkek/data/di/DataViewModelModule.kt
+++ app/src/main/java/com/stopsmoke/kekkek/data/di/DataViewModelModule.kt
@@ -14,6 +14,7 @@ import com.stopsmoke.kekkek.domain.repository.SearchRepository
 import com.stopsmoke.kekkek.firestore.dao.BookmarkRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 
