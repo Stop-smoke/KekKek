d2e49a30c475354abbd2ab9e942e434cba0ad4e0
agvber <agvber12@gmail.com>
2024-06-19 12:35:05 +0900

    feat: 프로필 이미지 업로드시에 progress dialog 추가

---
+++ app/src/main/java/com/stopsmoke/kekkek/presentation/settings/profile/ProfileImageUploadProgressFragment.kt
@@ -0,0 +1,46 @@
+package com.stopsmoke.kekkek.presentation.settings.profile
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.stopsmoke.kekkek.R
+import com.stopsmoke.kekkek.databinding.FragmentProfileImageUploadProgressBinding
+
+class ProfileImageUploadProgressFragment : DialogFragment() {
+    private var _binding: FragmentProfileImageUploadProgressBinding? = null
+    private val binding: FragmentProfileImageUploadProgressBinding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.TransparentBackgroundDialogFragmentStyle)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState).apply {
+            setCancelable(false)
+            setOnKeyListener { dia, keyCode, event ->
+                true
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentProfileImageUploadProgressBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        const val TAG = "ProfileImageUploadProgressFragment"
+    }
+}
