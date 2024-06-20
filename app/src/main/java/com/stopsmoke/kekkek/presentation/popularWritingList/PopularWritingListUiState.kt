91538ef56c531a14a18fb24fcdffe7fac77d8a43
yang dong won <didehddnjs89@gmail.com>
2024-06-20 10:41:42 +0900

    fix: 공지사항, 인기글 게시글 이동 밑 뒤로가기 시 글 refresh

--- app/src/main/java/com/stopsmoke/kekkek/presentation/popularWritingList/PopularWritingListUiState.kt
+++ app/src/main/java/com/stopsmoke/kekkek/presentation/popularWritingList/PopularWritingListUiState.kt
@@ -3,7 +3,8 @@ package com.stopsmoke.kekkek.presentation.popularWritingList
 import com.stopsmoke.kekkek.presentation.community.CommunityWritingItem
 
 data class PopularWritingListUiState (
-    val list: List<CommunityWritingItem>
+    val list: List<CommunityWritingItem>,
+    val isLoading: Boolean =false
 ) {
     companion object {
         fun init() = PopularWritingListUiState(
