d1bc68cdbce14ec27703e2f1ba3c62439e475984
agvber <agvber12@gmail.com>
2024-06-25 16:35:23 +0900

    chore: version code 2 -> 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.stopsmoke.kekkek"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
