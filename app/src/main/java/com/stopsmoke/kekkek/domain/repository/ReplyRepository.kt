29d66cc91169395288b41a2ca29caea38e47bbf2
agvber <agvber12@gmail.com>
2024-06-28 21:16:24 +0900

    feat: 대댓글 좋아요 추가 삭제 기능 추가

--- app/src/main/java/com/stopsmoke/kekkek/domain/repository/ReplyRepository.kt
+++ app/src/main/java/com/stopsmoke/kekkek/domain/repository/ReplyRepository.kt
@@ -1,16 +1,19 @@
 package com.stopsmoke.kekkek.domain.repository
 
 import androidx.paging.PagingData
-import com.stopsmoke.kekkek.domain.model.Comment
 import com.stopsmoke.kekkek.domain.model.Reply
 import kotlinx.coroutines.flow.Flow
 
 interface ReplyRepository {
     suspend fun addReply(reply: Reply): Result<Unit>
 
-    suspend fun getReply(postId: String, commentId: String): Flow<PagingData<Reply>>
+    suspend fun getReply(commentId: String): Flow<PagingData<Reply>>
 
     suspend fun deleteReply(reply: Reply)
 
     suspend fun updateReply(reply: Reply)
+
+    suspend fun appendReplyLike(postId: String, commentId: String, replyId: String)
+
+    suspend fun removeReplyLike(postId: String, commentId: String, replyId: String)
 }
