9eeeb2f4944fceccf618ed2d390e32c5a68d3ca8
agvber <agvber12@gmail.com>
2024-07-01 22:31:37 +0900

    design: 공백 recyclerview ui 추가

---
+++ app/src/main/java/com/stopsmoke/kekkek/presentation/post/viewholder/PostDeletedItemAdapter.kt
@@ -0,0 +1,6 @@
+package com.stopsmoke.kekkek.presentation.post.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.stopsmoke.kekkek.databinding.RecyclerviewEmptyBinding
+
+class PostDeletedItemAdapter(binding: RecyclerviewEmptyBinding): RecyclerView.ViewHolder(binding.root)
