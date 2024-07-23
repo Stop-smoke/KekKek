beeb3daa26320cc14df81d03deaeec8d3c41b41b
agvber <agvber12@gmail.com>
2024-07-23 22:35:37 +0900

    feat: FirebaseMessagingDataSource getToken 추가

---
+++ app/src/main/java/com/stopsmoke/kekkek/core/firemseeage/FirebaseMessagingDataSource.kt
@@ -0,0 +1,12 @@
+package com.stopsmoke.kekkek.core.firemseeage
+
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
+
+class FirebaseMessagingDataSource @Inject constructor(
+    private val messaging: FirebaseMessaging
+) {
+
+    suspend fun getToken(): String = messaging.token.await()
+}
