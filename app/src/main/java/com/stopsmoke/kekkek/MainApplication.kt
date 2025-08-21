c0c6656233264ba8e2e44dab94f920e592179fbb
agvber <agvber12@gmail.com>
2025-08-21 11:05:21 +0900

    feat: 디버그 해시키 로깅 추가

--- app/src/main/java/com/stopsmoke/kekkek/MainApplication.kt
+++ app/src/main/java/com/stopsmoke/kekkek/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.stopsmoke.kekkek
 
 import android.app.Application
+import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
@@ -9,7 +10,11 @@ class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_API_KEY)
+        Log.d(TAG, "KeyHash ${KakaoSdk.keyHash}")
+    }
+
+    companion object {
+        const val TAG: String = "MainApplication"
     }
 }
