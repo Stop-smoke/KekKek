c09eb559339ec032716cc50336cff3ea18c1ee4c
agvber <agvber12@gmail.com>
2025-08-21 15:09:54 +0900

    refactor: MainApplication code cleanup

--- app/src/main/java/com/stopsmoke/kekkek/MainApplication.kt
+++ app/src/main/java/com/stopsmoke/kekkek/MainApplication.kt
@@ -2,8 +2,6 @@ package com.stopsmoke.kekkek
 
 import android.app.Application
 import android.util.Log
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
@@ -14,10 +12,6 @@ class MainApplication : Application() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_API_KEY)
         Log.d(TAG, "KeyHash ${KakaoSdk.keyHash}")
-
-        Firebase.auth.currentUser?.providerData?.forEach {
-            Log.d(TAG, it.providerId)
-        }
     }
 
     companion object {
